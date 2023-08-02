8870e831b64e0535293f1fc0a4761583af1b5731
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-08-02 08:17:58 -0600

    server updates to link schema files

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,6 +1,9 @@
+
+//refference to documentation on web-vitals 2.1.4
+//web-vitals/base and import dist/polyfill.js into head of the pages
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals/base').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
@@ -8,6 +11,6 @@ const reportWebVitals = onPerfEntry => {
       getTTFB(onPerfEntry);
     });
   }
-};
+}
 
 export default reportWebVitals;
