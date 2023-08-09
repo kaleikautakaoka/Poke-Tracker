67a479c0fa347b8a5905a450e46f117b709cb46c
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-08-09 00:44:14 -0600

    localstorage

---
+++ server/utils/localStorage.js
@@ -0,0 +1,14 @@
+export const getPokemonData = () => {
+    //fetch the data and return it to local storage
+    const savedPokemon = localStorage.getItem('pokemon')
+    ? JSON.parse(localStorage.getItem(savedPokemon)) : []; 
+        return savedPokemon;
+    };
+
+
+export const getUserData = () => {
+    //fetch the data and return it to local storage
+    const savedUser = localStorage.getItem('user')
+    ? JSON.parse(localStorage.getItem(savedUser)) : []; 
+        return savedUser;
+    }
