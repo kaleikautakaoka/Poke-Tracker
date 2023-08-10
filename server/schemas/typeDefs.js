d47e3bd0edc36b8e5828c0d6c0d9d2af0df42d25
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-08-10 11:25:46 -0600

    redoing type defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,46 +16,11 @@ const typeDefs = gql`
         category: Category
     }  
 
-    type Gen3_species {
-         name: String
-        _id: ID
-    }
-
-    type Generations {
-        name: String
-        _id: ID
-    }
-
-    type Gen1_species {
-        name: String
-        _id: ID
-    }
-
-    type Gen2_species {
-        name: String
-       _id: ID
-    }
-
-    type Gen4_species {
-        name: String
-        _id: ID
-    }
-
-    type Gen5_species {
-        name: String
-        _id: ID
-    }
-
-    type TestPokeApidata {
-        name: String
-        _id: ID
-    }
-
     type User {
         _id: ID
         username: String
         email: String
-        savedpokemon: [SavedPokemon]
+        pokemons: [Pokemon]
     }
 
     type Auth {
@@ -68,34 +33,14 @@ const typeDefs = gql`
         pokemons(category: ID, name: String): [Pokemon]
         pokemon(_id: ID!): Pokemon
         user: User
-        gen3_species: [gen3_species]
-        generations: [generations]
-        gen1_species: [gen1_species]
-        gen2_species: [gen2_species]
-        gen4_species: [gen4_species]
-        gen5_species: [gen5_species]
-        generations: TestPokeApidata
 
     }
 
     type Mutation {
         addUser(username: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
-        
-        updateUser(username: String, email: String, password: String): User
-
-        savePokemon(pokemon: [ID]!): SavedPokemon
-        removePokemon(pokemon: ID!): SavedPokemon
-
-        removeUser: User
-
-        gen3_species: [gen3_species]
-        generations: [generations]
-        gen1_species: [gen1_species]
-
-        addPokeTest(name: String!, id: ID!): TestPokeApidata
-
-
+        updatPokemon(pokemonId: ID!, quantity: Int!): Pokemon
+        login(email: String!, password: String!): Auth
     }
         
 `;
